6cb0e73db8e26fb71fe9c7a61cfe8ce5699069f5
dean <diazdean21@gmail.com>
2021-09-06 12:58:58 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = 62;
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+var random = Math.floor(Math.random() * 10) + 1;
 
-// Write your code here
+let num3 = 1;
+let num4 = 3;
+let mod = 4;
+function mod(num3, num4, mod){
+    return num3 + num4;
+}
+
+var max = 20;
+for (var i = 0; i < Array.length; i++) {
+    if (arr[i] < max){
+        max = arr [i];
+        console.log ('setting max to'+ min);
+    }
+}
