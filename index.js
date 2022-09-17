0e59fee685d6979998490855b8c82273ab4e3b55
dean <diazdean21@gmail.com>
2022-09-17 14:56:30 -0500

    commit tree 25221c8a6b912ae15ac4893fde3d09b24a8b9d0e

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 let num1 = 31;
-let num2 = 2;
+let num2 = 31;
 let multiply = 62;
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
-var random = Math.floor(Math.random() * 10) + 1;
+
+let random = Math.floor(Math.random() * 10) + 1;
 
 let num3 = 1;
 let num4 = 3;
 let mod = 4;
-function mod(num3, num4, mod){
-    return num3 + num4;
+
+function mod(num3, num4, mod) {
+  return num3 + num4;
 }
 
-var max = 20;
-for (var i = 0; i < Array.length; i++) {
-    if (arr[i] < max){
-        max = arr [i];
-        console.log ('setting max to'+ min);
-    }
-}
+// let num1 = 31;
+// let num2 = 2;
+// let multiply = 62;
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+// var random = Math.floor(Math.random() * 10) + 1;
+
+// let num3 = 1;
+// let num4 = 3;
+// let mod = 4;
+// function mod(num3, num4, mod){
+//     return num3 + num4;
+// }
+
+// var max = 20;
+// for (var i = 0; i < Array.length; i++) {
+//     if (arr[i] < max){
+//         max = arr [i];
+//         console.log ('setting max to'+ min);
+//     }
+// }
